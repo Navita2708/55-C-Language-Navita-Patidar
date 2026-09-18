2bc0c4f1707381f8d9209e37e37b065357ea4ecd
Navita Patidar <navitaiips@gmail.com>
2026-09-18 08:33:41 +0530

    Add files via upload

---
+++ LAB_03/Less_then.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5;
+    int b=10;
+    printf("Navita Patidar\n");
+    printf("%d", a<b);
+    return 0;
+}
+
---
+++ LAB_03/Not_equal_to.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=25;
+    int b=20;
+    printf("Navita Patidar\n");
+    printf("%d", a!=b);
+    return 0;
+}
